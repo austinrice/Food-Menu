9f30efa2c002990e7a6f1df20406fbbcbb661881
-Austin Rice <austinhusker@gmail.com>
2012-03-05 08:25:03 -0700

    Initial Commit

---
+++ TrialStuff/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  TrialStuff
+//
+//  Created by Austin Rice on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ TrialStuff/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  TrialStuff
+//
+//  Created by Austin Rice on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
